0fa127b060d35cd5c00aa0e2e84eb4c81e6fafc8
Michael Benedetti <michael.d.benedetti@gmail.com>
2021-04-15 22:02:06 +0000

    Added kube-prometheus-stack

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 # k3s install
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik --disable servicelb" sh - 
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+chmod 600 ~/.kube/config
 
 while kubectl get node | grep NotReady; do echo "Waiting for k3s..."; sleep 1; done
 
@@ -21,3 +22,12 @@ echo "Deploying heimdall..."
 kubectl create namespace heimdall
 while ! kubectl -n heimdall get serviceaccount | grep default; do echo "Waiting for service account to be ready..."; sleep 1; done
 kubectl apply -f yaml/heimdall/heimdall.yaml --namespace=heimdall
+
+# prometheus/grafana
+kubectl create namespace prometheus
+while ! kubectl -n prometheus get serviceaccount | grep default; do echo "Waiting for service account to be ready..."; sleep 1; done
+echo "Deploying prometheus/grafana..."
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install -n prometheus kube-prometheus-stack prometheus-community/kube-prometheus-stack
+kubectl -n prometheus patch svc "kube-prometheus-stack-grafana" -p '{"spec": {"type": "LoadBalancer", "loadBalancerIP": "192.168.1.19"}}'
